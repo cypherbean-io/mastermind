d2093aafbdfebff636e6c2fab637c9284051d4b1
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-09-05 16:37:17 -0500

    Fix instance variables in #play

--- lib/game.rb
+++ lib/game.rb
@@ -11,8 +11,8 @@ class Game
     @max_turns.times do |turn|
       puts "\nTurn #{turn + 1} / #{@max_turns}"
       @board.display
-      guess = code_breaker.make_guess
-      feedback = code_maker.provide_feedback(guess, @secret_code)
+      guess = @code_breaker.make_guess
+      feedback = @code_maker.provide_feedback(guess, @secret_code)
       @board.update(turn, guess, feedback)
       break if guess == @secret_code
     end
