528250c4c4136dc12c1c05230552ba571098d0d3
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-09-04 19:38:43 -0500

    Add CodeBreaker class with #make_guess and #valid_guess? methods

---
+++ lib/code_breaker.rb
@@ -0,0 +1,17 @@
+class CodeBreaker
+  def initialize; end
+
+  def valid_guess?(guess)
+    guess.length == 4 && guess.all? { |num| '1'..'6'.include?(num) }
+  end
+
+  def make_guess
+    loop do
+      print 'Enter your guess (4 numbers between 1 and 6, space-separated): '
+      guess = gets.chomp.split
+      return guess if valid_guess?(guess)
+
+      puts 'Invalid guess. Try again.'
+    end
+  end
+end
