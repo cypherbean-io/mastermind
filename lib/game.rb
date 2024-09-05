955e0a562d6b0567f726a85438887bbbed67008c
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-09-05 16:08:12 -0500

    Update #play and remove #display_feedback

--- lib/game.rb
+++ lib/game.rb
@@ -4,21 +4,17 @@ class Game
     @code_breaker = CodeBreaker.new
     @secret_code = @code_maker.generate_code
     @max_turns = 12
+    @board = Board.new
   end
 
   def play
     @max_turns.times do |turn|
       puts "\nTurn #{turn + 1} / #{@max_turns}"
+      @board.display
       guess = code_breaker.make_guess
       feedback = code_maker.provide_feedback(guess, @secret_code)
-      display_feedback(feedback)
+      @board.update(turn, guess, feedback)
       break if guess == @secret_code
     end
-  end
-
-  private
-
-  def display_feedback(feedback)
-    puts "Exact matches: #{feedback[:exact]}, Color matches: #{feedback[:color]}"
   end
 end
